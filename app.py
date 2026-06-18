32257f25d86bc25ed928c0940eeb61492d7c411a
Gaby Nieva <gaby.nieva@outlook.com>
2026-06-18 10:11:12 -0600

    Libro DAO completo

---
+++ app.py
@@ -0,0 +1,19 @@
+from dao.libro_dao import LibroDAO
+
+def main():
+    try:
+        libro_dao = LibroDAO()
+        libros = libro_dao.obtener_todo()
+
+        if len(libros) == 0:
+            print("No hay libros registrado")
+        else:
+            for libro in libros:
+                print(f" {libro.id} {libro.titulo} { libro.autor} {libro.isbn} {libro.disponible}" )
+        print("\n Conexion exitosa con la base de datos")
+    except Exception as e:
+        print("Error")
+        print(e)
+
+if __name__ == "__main__":
+    main()
--- database/conexion.py
+++ database/conexion.py
@@ -11,7 +11,7 @@ class Conexion:
     def obtener_conexion():
         return psycopg2.connect(
             host=os.getenv("DB_HOST"),
-            database=os.getenv("DB-NAME"),
+            database=os.getenv("DB_NAME"),
             user=os.getenv("DB_USER"),
             password=os.getenv("DB_PASSWORD"),
             port=os.getenv("DB_PORT")
